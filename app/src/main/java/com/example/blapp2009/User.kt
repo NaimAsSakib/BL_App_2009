b49870d2f51020e31cbe085330e9ab94dadc928a
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-11-19 20:06:54 +0600

    Added RegisterProfileActivity.kt, added realtime database library in build.gradle

---
+++ app/src/main/java/com/example/blapp2009/User.kt
@@ -0,0 +1,4 @@
+package com.example.blapp2009
+
+data class User(var name: String?=null, var location: String?=null, var occupation: String?=null, var organization: String?=null,
+                var number1: String?=null, var number2: String?=null)
