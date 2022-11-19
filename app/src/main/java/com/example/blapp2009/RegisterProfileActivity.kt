cf688ddf6075ebe58a4f6bbd85edd1089f8d5d4f
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-11-19 21:27:17 +0600

    Added viewBinding() to get xml element ids directly from activity Added realtime database dependecy and codes in Regis

--- app/src/main/java/com/example/blapp2009/RegisterProfileActivity.kt
+++ app/src/main/java/com/example/blapp2009/RegisterProfileActivity.kt
@@ -2,12 +2,29 @@ package com.example.blapp2009
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.blapp2009.databinding.ActivityRegisterProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterProfileBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_profile)
+       // setContentView(R.layout.activity_register_profile)
+        binding= ActivityRegisterProfileBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
+        auth= FirebaseAuth.getInstance()
+        val uid= auth.currentUser?.uid
+
+
+        databaseReference=FirebaseDatabase.getInstance().getReference("")
 
     }
 }
