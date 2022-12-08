13e22fe9bc776819a50ceae83b81f7d32a4fc3f9
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-12-08 12:23:46 +0600

    Added single selection & dis-selection mechanism for bloodGroup in FragFilterBloodRCVAdapter.kt

--- app/src/main/java/com/example/blapp2009/FragFilterBloodRCVAdapter.kt
+++ app/src/main/java/com/example/blapp2009/FragFilterBloodRCVAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.blapp2009
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import java.util.*
-import kotlin.collections.ArrayList
 
 class FragFilterBloodRCVAdapter(private val bloodGroupArrayList: ArrayList<String> ):
 RecyclerView.Adapter<FragFilterBloodRCVAdapter.MyViewHolder>() {
+
+    var rcvRowIndex = -1 //For changing textview shape color when clicked & for selecting one at a time
+    var hashMap = HashMap<Int, String>() //for selecting & unselecting mechanism
+
 
     class MyViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvBloodGroup: TextView =itemView.findViewById(R.id.tvBloodGroupRCVFragFilter)
@@ -23,8 +26,28 @@ RecyclerView.Adapter<FragFilterBloodRCVAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.tvBloodGroup.text=  bloodGroupArrayList[position]
+
+
+        //condition for single selection & dis-selection of the bloodGroup from, here to the end of  holder.tvBloodGroup.setOnClickListener
+        if (hashMap.containsKey(position)) {
+            holder.tvBloodGroup.setBackgroundResource(R.drawable.circle_shape_blood_red)
+            holder.tvBloodGroup.setTextColor(Color.WHITE)
+        } else {
+            holder.tvBloodGroup.setBackgroundResource(R.drawable.circle_shape_blood_normal)
+            holder.tvBloodGroup.setTextColor(Color.parseColor("#536DFE"))
+        }
+
         holder.tvBloodGroup.setOnClickListener {
-            holder.tvBloodGroup.text=  bloodGroupArrayList[position]
+            //when user select a textview
+            if (!hashMap.containsKey(position)) {
+                hashMap.clear()
+                hashMap.put(position,holder.tvBloodGroup.toString())
+
+            } else { //when user unselect a radiobutton
+                hashMap.remove(position)
+
+            }
+            notifyDataSetChanged()
         }
     }
 
