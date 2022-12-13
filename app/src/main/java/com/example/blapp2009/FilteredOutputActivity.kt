fafbc3452ef040996a060ff50c1b087623a28487
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-12-13 22:17:49 +0600

    Took new activity FilteredOutputActivity.kt & passed name value from user input from FragFilterName.kt to FilteredOutputActivity.kt

---
+++ app/src/main/java/com/example/blapp2009/FilteredOutputActivity.kt
@@ -0,0 +1,16 @@
+package com.example.blapp2009
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class FilteredOutputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtered_output)
+
+        //getting value from FragFilterName
+        val intent = intent
+        val nameFromFragFilterName = intent.getStringExtra("inputNameFromFragFilterName")
+    }
+}
--- app/src/main/java/com/example/blapp2009/FragFilterName.kt
+++ app/src/main/java/com/example/blapp2009/FragFilterName.kt
@@ -1,26 +1,47 @@
 package com.example.blapp2009
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.DatabaseReference
-import java.util.ArrayList
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 class FragFilterName : Fragment() {
 
-
+    private lateinit var etName: EditText
+    private lateinit var btnSearch: Button
+    private lateinit var name: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_frag_filter_name, container, false)
+        val view = inflater.inflate(R.layout.fragment_frag_filter_name, container, false)
 
+        etName = view.findViewById(R.id.etFragFilterName)
+        btnSearch = view.findViewById(R.id.btnFragName)
+
+
+        btnSearch.setOnClickListener {
+
+            name = etName.text.toString()
+
+            //checking whether name field is empty or not
+            if (name.trim().isNotEmpty()) {
+                val intent = Intent(context, FilteredOutputActivity::class.java)
+                intent.putExtra("inputNameFromFragFilterName", name)
+                startActivity(intent)
+            } else {
+                Toast.makeText(context, "Please enter a name", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
         return view
