78649d1adca8be3fc7cc2b73e2125eb9bbdef3ff
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2023-01-03 23:41:40 +0600

    Made progressbar loader going though user touch the screen

--- app/src/main/java/com/example/blapp2009/LoadingProgressBarDialog.kt
+++ app/src/main/java/com/example/blapp2009/LoadingProgressBarDialog.kt
@@ -12,7 +12,8 @@ class LoadingProgressBarDialog(private val activity: Activity) {
         val builder = AlertDialog.Builder(activity)
         val inflater = activity.layoutInflater
         builder.setView(inflater.inflate(R.layout.circular_progressbar_dialog, null))
-        builder.setCancelable(true)
+        //builder.setCancelable(true)  //'true' means if user touch screen while loading, loader disappears
+        builder.setCancelable(false)   //'false' means if user touch screen while loading, loader continue loading
 
         alertDialog = builder.create()
         alertDialog!!.show()
